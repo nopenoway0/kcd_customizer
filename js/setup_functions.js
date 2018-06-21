5850b4924e6ce1ba254474f34e9fd9f63795d112
nopenoway0 <benjamin.gottheil@sjsu.edu>
2018-06-20 23:09:11 -0700

    fixed export

--- js/setup_functions.js
+++ js/setup_functions.js
@@ -1,14 +1,16 @@
-var fs = require('fs')
+var fs = require('fs-extra')
 var xml2js = require('xml2js')
 var THREE = require('three')
 require('./js/LoaderSupport.js')
 require('./js/OBJLoader2.js')
+var JSZip = require('jszip')
+
 function create_texture_database(filelist)
 {
 	return new Promise((resolve, reject) => {
 		let parser = new xml2js.Parser();
 		let re = /.*\/(.*)\..*$/i
-		var model_textures = [];
+		var model_textures = {'model':[], 'head':{}, 'eyes':{}, 'mouth':{}, 'beard':{}, 'hair':{}};
 		for(var x = 0; x < filelist.length; x++){
 			let data = fs.readFileSync(filelist[x]);
 			let mats = [];
@@ -20,13 +22,16 @@ function create_texture_database(filelist)
 				for(var y = 0; y < materials.length; y++)
 				{
 					if(materials[y]['$'].Name == 'head' || materials[y]['$'].Name == 'eyes' || materials[y]['$'].Name == 'mouth' || materials[y]['$'].Name == 'beard' || materials[y]['$'].Name == 'hair')
+					{
 						mats.push('textures/' + materials[y]["Textures"][0]["Texture"][0]['$'].File.match(re)[1] + ".jpg");
+						model_textures[materials[y]['$'].Name]['textures/' + materials[y]["Textures"][0]["Texture"][0]['$'].File.match(re)[1] + ".jpg"] = true;
+					}
 				}
 				} catch(err)
 				{
 					return;
 				}
-				model_textures.push(mats);	
+				model_textures['model'].push(mats);	
 			});
 		}
 		resolve(model_textures);
@@ -79,7 +84,6 @@ function get_file_list(directory, re, include_path = true)
 function load_asset(filename, type = 'obj', params = null){
 	return new Promise((resolve, reject) => {
 	let loader = null;
-	console.log(THREE);
 	if(type == 'obj')
 	{
 		loader = new THREE.OBJLoader2()
@@ -104,5 +108,72 @@ function load_asset(filename, type = 'obj', params = null){
 		console.log(err);
 		reject(err);
 	});
+	});
+}
+
+function copy(file1, location)
+{
+	return new Promise((res, rej) =>{
+		fs.copy(file1, location, ()=>{
+			res();
+		});
+	});
+}
+
+function mkdir(dir)
+{
+	return new Promise((res, rej) =>{
+		if(!fs.exists(dir))
+		{
+			fs.mkdir(dir, (err)=>{
+					if(err != null)
+						rej();
+					res();
+			});
+		}
+		else
+			rej(dir + ' exists!');
+	});
+}
+
+function rename(old_path, new_path)
+{
+	return new Promise((res, rej) => {
+		if(fs.exists(old_path)){
+			fs.rename(old_path, new_path, (err)=>{
+				if(err != null)
+					rej('error occured' + err);
+				else
+					res('renamed ' + old_path + ' to ' + new_path);
+			});
+		}
+	});
+}
+
+function rmdir(dir)
+{
+	return new Promise((res, rej) =>{
+	if(fs.exists(dir))
+		fs.remove(dir, ()=>{
+			console.log(dir + " removed");
+			res();
+		});
+	})
+}
+
+function zipFolder(dir, name, files, path = "contents")
+{
+	return new Promise((res)=>{
+	console.log(files);
+	let zip = new JSZip();
+	let folder = zip.folder(path);
+	for(let x = 0; x < files.length; x++){
+		let contents = fs.readFileSync(dir + files[x])
+		folder.file(files[x], contents, {binary:true});
+	}
+	res(zip.generateAsync({type: "uint8array"}).then((data)=>{
+		console.log('trying to write to file');
+		fs.writeFileSync(name, data);
+	}));
 	});
 }
