684b1a998b16e8dda962d47fdeef6fdc815e07b7
nopenoway0 <benjamin.gottheil@sjsu.edu>
2018-06-22 00:53:54 -0700

    setup ipc main to recieve messages from render

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
   
   function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 730, height: 600})
-  
-    // and load the index.html of the app.
+  // Create the browser window.
+    let win = new BrowserWindow({width: 730, height: 600})
     win.loadFile('index.html')
+
+    let setup_window = new BrowserWindow({width: 400, height: 200, show: false});
+    setup_window.loadFile('setup_index.html')
+    
+    ipcMain.on('show_setup_window', (event, data) =>{
+      setup_window.show();
+    });
+
+    ipcMain.on('directory_set', (event, path) =>
+    {
+      win.webContents.send('directory_set', path);
+      win.webContents.send('resume_render');
+    });
+
+    // and load the index.html of the app.
   }
   
   app.on('ready', createWindow)
