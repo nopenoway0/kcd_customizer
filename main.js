675d68553411067fedf67c15d359eba644f93747
nopenoway0 <benjamin.gottheil@sjsu.edu>
2018-06-25 04:18:23 -0700

    added trigger to resume after setup is finished

--- main.js
+++ main.js
@@ -27,6 +27,12 @@ const {app, BrowserWindow, ipcMain} = require('electron')
       win.webContents.send('resume_render');
     });
 
+    // might need to modify this
+    ipcMain.on('resume', (event, data) =>{
+      setup_window.hide();
+      win.webContents.send('resume_render');
+    })
+
     ipcMain.on('close_setup_window', (event, data) =>
     {
       if(setup_window != null)
